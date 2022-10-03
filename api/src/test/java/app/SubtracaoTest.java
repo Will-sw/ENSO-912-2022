f45b46fe57e0d9a01f47fa01dfe58070b42fc64f
Gabriel Anderson <gabrielandersosilva@gmail.com>
2022-10-03 11:28:41 +0000

    Indentação corrigida!

--- api/src/test/java/app/SubtracaoTest.java
+++ api/src/test/java/app/SubtracaoTest.java
@@ -1,28 +1,27 @@
 package app;
 
 import org.junit.jupiter.api.Test;
- 
 import static org.junit.jupiter.api.Assertions.assertEquals;
- 
 import org.junit.jupiter.api.BeforeEach;
- 
+
 public class SubtracaoTest {
-   
+
     Subtracao sub;
-   
+
     @BeforeEach
-    public void Setup(){
+    public void Setup() {
         sub = new Subtracao();
     }
+
     @Test
-    public void VerificarCasoBase(){
-        double resultado = sub.calcula("7","5");
-        assertEquals(2.0,resultado);
+    public void VerificarCasoBase() {
+        double resultado = sub.calcula("7", "5");
+        assertEquals(2.0, resultado);
     }
- 
+
     @Test
-    public void VerificarSubtracaoNumeroNegativ(){
-        double resultado = sub.calcula("-7","2");
-        assertEquals(-9.0,resultado);
+    public void VerificarSubtracaoNumeroNegativ() {
+        double resultado = sub.calcula("-7", "2");
+        assertEquals(-9.0, resultado);
     }
 }
