22c3572a143b4e23734887fb5b0d01653cc75e14
kelven1606 <kfst1@aluno.ifal.edu.br>
2022-10-03 11:17:00 +0000

    adicionando teste de unidade para Multiplicacao

---
+++ api/src/test/java/app/MultiplicacaoTest.java
@@ -0,0 +1,27 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class MultiplicacaoTest {
+    
+    Multiplicacao mult;
+    
+    @BeforeEach
+    public void Setup(){
+        mult = new Multiplicacao();
+    }
+    @Test
+    public void VerificarCasoBase(){
+        double resultado = mult.calcula("2","2");
+        assertEquals(4.0,resultado);
+    }
+
+    @Test
+    public void VerificarMultiplicacaoNumeroNegativ(){
+        double resultado = mult.calcula("-2","2");
+        assertEquals(-4.0,resultado);
+    }
+}
